680a5e2cf25686a1ee1e7ffce7d4af8df7f8f45d
HaroonCheema <haroon051118@gmail.com>
2023-11-10 12:06:24 +0500

    ok

--- src/config.php
+++ src/config.php
@@ -17,4 +17,4 @@ $config['designer']['enabled'] = true;
 
 $config['fullUnicode'] = true;
 
-$config['development']['defaultAddOn'] = 'FS/HideUsernames';
+$config['development']['defaultAddOn'] = 'FS/DetectMoblieRedirect';
