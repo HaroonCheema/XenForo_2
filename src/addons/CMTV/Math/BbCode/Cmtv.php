7776baed52f8edd58f020b59e89aac75d59f4929
HaroonCheema <haroon051118@gmail.com>
2024-03-11 12:54:15 +0500

    ok

--- src/addons/CMTV/Math/BbCode/Cmtv.php
+++ src/addons/CMTV/Math/BbCode/Cmtv.php
@@ -6,7 +6,6 @@ class Cmtv
 {
     public static function renderTagCmtv($tagChildren, $tagOption, $tag, array $options, \XF\BbCode\Renderer\AbstractRenderer $renderer)
     {
-<<<<<<< HEAD
         // $content = $renderer->renderSubTree($tagChildren, $options);
 
         // if ($content === '') {
@@ -14,13 +13,7 @@ class Cmtv
         // }
 
         $empty = "";
-=======
 
-        $empty = "\begin{align*}
-        \int_a^b f(x) dx &= \lim_{n \to \infty} \sum_{i=1}^n f(a + i \Delta x) \Delta x \\
-        \frac{d}{dx} \sin(x) &= \cos(x)
-        \end{align*}";
->>>>>>> ec84cb00a69fdecad2b7ec284b83c9f007c870e0
 
         $viewParams = [
             'equation' => isset($tagChildren[0]) ? $tagChildren[0] : $empty
@@ -31,7 +24,6 @@ class Cmtv
 
     public static function renderTagCmtvs($tagChildren, $tagOption, $tag, array $options, \XF\BbCode\Renderer\AbstractRenderer $renderer)
     {
-<<<<<<< HEAD
         // $content = $renderer->renderSubTree($tagChildren, $options);
 
         // if ($content === '') {
@@ -39,13 +31,7 @@ class Cmtv
         // }
 
         $empty = "";
-=======
 
-        $empty = "\begin{align*}
-        \int_a^b f(x) dx &= \lim_{n \to \infty} \sum_{i=1}^n f(a + i \Delta x) \Delta x \\
-        \frac{d}{dx} \sin(x) &= \cos(x)
-        \end{align*}";
->>>>>>> ec84cb00a69fdecad2b7ec284b83c9f007c870e0
 
         $viewParams = [
             'equation' => isset($tagChildren[0]) ? $tagChildren[0] : $empty
