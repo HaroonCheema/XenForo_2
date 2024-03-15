3335b6aa0d21da145e77880c2a77305ae288291f
HaroonCheema <haroon051118@gmail.com>
2024-03-15 16:53:35 +0500

    ok

--- internal_data/code_cache/templates/l1/s0/public/CMTV_Math_insert_math_jax_dialog.php
+++ internal_data/code_cache/templates/l1/s0/public/CMTV_Math_insert_math_jax_dialog.php
@@ -1,5 +1,5 @@
 <?php
-// FROM HASH: 8c4a74f8caf990ac217509d601aff96b
+// FROM HASH: a3cd14b974b4b8d28632e89d6dbb0be5
 return array(
 'code' => function($__templater, array $__vars, $__extensions = null)
 {
@@ -43,7 +43,21 @@ return array(
 	), array(
 		'label' => 'LaTeX input',
 		'explain' => '<a href="https://www.latex-tutorial.com/tutorials/amsmath/" target="_blank">How to write math using LaTeX?</a> • <a href="https://katex.org/docs/supported.html" target="_blank">List of supported functions</a>',
-	)) . '			
+	)) . '
+
+			<h2 class="block-formSectionHeader">
+				<span class="block-formSectionHeader-aligner">
+					<span class="preview-loading">' . $__templater->fontAwesome('fa-sync-alt fa-spin', array(
+	)) . '</span>
+					' . 'Preview' . '
+				</span>
+			</h2>
+
+			<div class="preview-container showing">
+				<div id="mathPreview"><formula id="preview">' . 'Preview' . '</formula></div>
+				<div class="error" id="error">' . 'Incorrect latex input!' . '</div>
+				<div class="empty" id="empty">' . 'Start typing math in "LaTeX input"...' . '</div>
+			</div>
 		</div>
 
 		' . $__templater->formSubmitRow(array(
@@ -52,7 +66,72 @@ return array(
 	), array(
 	)) . '
 	</div>
-</form>';
+</form>
+
+<script>
+	const formulaInput = document.getElementById("latex_input");
+	const outputDiv = document.getElementById("preview");
+	const emptyDiv = document.getElementById("empty");
+	const previewDiv = document.getElementById("mathPreview");
+	const errorDiv = document.getElementById("error");
+	const loadingIcon = document.querySelector(".preview-loading");
+
+	previewDiv.style.display = "none";
+	errorDiv.style.display = "none";
+	outputDiv.textContent = "";
+
+	function showLoadingIcon() {
+		loadingIcon.classList.add("showing");
+	}
+
+	function hideLoadingIcon() {
+		loadingIcon.classList.remove("showing");
+	}
+
+	function convertFormula() {
+
+		const formula = formulaInput.value;
+		outputDiv.textContent = "";
+		showLoadingIcon();
+
+		// Regular expression to match LaTeX delimiters or commands
+		const mathJaxPattern = /\\\\\\[|\\\\\\]|\\\\\\(|\\\\\\)|\\\\(?:[^a-zA-Z]|[a-zA-Z]+ ?)/;
+		if (mathJaxPattern.test(formula)) {
+
+			previewDiv.style.display = "block";
+			emptyDiv.style.display = "none";
+			errorDiv.style.display = "none";
+
+			var mathPreviewDiv = document.getElementById("mathPreview");
+
+			var formulaElement = document.createElement("formula");
+
+			formulaElement.textContent = formula;
+
+			mathPreviewDiv.innerHTML = "";
+			mathPreviewDiv.appendChild(formulaElement);
+
+			hideLoadingIcon();
+
+			var preview = document.getElementsByClassName("mathJaxEqu");
+			MathJax.typeset(preview);
+
+		} else {
+			if (formulaInput.value.trim() === "") {
+				emptyDiv.style.display = "block";
+				errorDiv.style.display = "none";
+			} else {
+				emptyDiv.style.display = "none";
+				errorDiv.style.display = "block";
+			}
+			previewDiv.style.display = "none";
+
+			hideLoadingIcon();
+		}
+	}
+
+	formulaInput.addEventListener("input", convertFormula);
+</script>';
 	return $__finalCompiled;
 }
 );
