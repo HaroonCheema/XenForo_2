1e5124be774173971faf6a5476f3d621709b3312
HaroonCheema <haroon051118@gmail.com>
2025-02-24 12:08:29 +0500

    ok

--- src/addons/FS/ClaimEmails/_output/extension_hint.php
+++ src/addons/FS/ClaimEmails/_output/extension_hint.php
@@ -3,11 +3,6 @@
 // ################## THIS IS A GENERATED FILE ##################
 // DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
 
-namespace FS\ClaimEmails\XF\Admin\Controller
-{
-	class XFCP_User extends \XF\Admin\Controller\User {}
-}
-
 namespace FS\ClaimEmails\XF\Entity
 {
 	class XFCP_User extends \XF\Entity\User {}
